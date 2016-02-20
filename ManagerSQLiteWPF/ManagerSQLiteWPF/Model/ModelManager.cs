21e189666baa417cf8c8391c0bf7365bf5acfe12
Diego <luarca84@gmail.com>
2016-02-20 10:42:21 +0100

    Added files

---
+++ ManagerSQLiteWPF/ManagerSQLiteWPF/Model/ModelManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerSQLiteWPF.Model
+{
+    class ModelManager
+    {
+        static ModelManager instance = new ModelManager();
+        SQLiteConnection m_dbConnection;
+
+        private ModelManager() { }
+
+        internal static ModelManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+
+            set
+            {
+                instance = value;
+            }
+        }
+
+        public void ConnectToDataBase()
+        {            
+            m_dbConnection = new SQLiteConnection("Data Source=DataBase/granja.sqlite;Version=3;");
+            m_dbConnection.Open();
+        }
+
+        public List<string> GetLstVacas()
+        {
+            List<string> lstVacas = new List<string>();
+            string sql = "select Nombre from vacas";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                lstVacas.Add(reader["Nombre"].ToString());
+            }
+            return lstVacas;
+        }
+    }
+}
---
+++ ManagerSQLiteWPF/ManagerSQLiteWPF/ViewModel/ViewModelBase.cs
@@ -0,0 +1,35 @@
+﻿using ManagerSQLiteWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerSQLiteWPF
+{
+    class ViewModelBase
+    {
+        List<string> lstVacas = new List<string>();
+
+        public ViewModelBase()
+        {
+            ModelManager.Instance.ConnectToDataBase();
+            lstVacas = ModelManager.Instance.GetLstVacas();         
+        }
+
+        public List<string> LstVacas
+        {
+            get
+            {
+                return lstVacas;
+            }
+
+            set
+            {
+                lstVacas = value;
+            }
+        }
+    }
+}
